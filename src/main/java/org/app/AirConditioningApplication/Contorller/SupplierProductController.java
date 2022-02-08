2b32df21d45aba977d22e4155cbf5200bc6999ad
Talha Farooq <hafiztalha1997@gmail.com>
2022-02-08 13:36:03 +0500

    Supplier Products "CRUD" is created

---
+++ src/main/java/org/app/AirConditioningApplication/Contorller/SupplierProductController.java
@@ -0,0 +1,39 @@
+package org.app.AirConditioningApplication.Contorller;
+
+import org.app.AirConditioningApplication.Model.SupplierProduct;
+import org.app.AirConditioningApplication.Service.SupplierProductService;
+import org.app.AirConditioningApplication.response.ApiResponse;
+import org.springframework.web.bind.annotation.*;
+
+public class SupplierProductController {
+    private final SupplierProductService supplierProductService;
+
+    public SupplierProductController(SupplierProductService supplierProductService) {
+        this.supplierProductService = supplierProductService;
+    }
+
+    @GetMapping("/list")
+    public ApiResponse list() {
+        return supplierProductService.showAll();
+    }
+
+    @PostMapping("/add")
+    public ApiResponse add(@RequestBody SupplierProduct supplierProduct) {
+        return supplierProductService.save(supplierProduct);
+    }
+
+    @PutMapping("/update")
+    public ApiResponse update(@RequestBody SupplierProduct supplierProduct) {
+        return supplierProductService.save(supplierProduct);
+    }
+
+    @DeleteMapping("/delete")
+    public ApiResponse delete(@RequestParam Long Id) {
+        return supplierProductService.delete(Id);
+    }
+
+    @GetMapping("/getByID")
+    public ApiResponse getById(@RequestParam Long Id) {
+        return supplierProductService.getById(Id);
+    }
+}
